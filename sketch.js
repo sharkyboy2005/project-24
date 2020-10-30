dc05a980a9ecbe156f4988deae6cea233a5b8f07
sharkyboy2005 <70273799+sharkyboy2005@users.noreply.github.com>
2020-10-30 17:12:53 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var ground;
+var ground, ball, ballZone;
 
 function preload()
 {
@@ -19,7 +19,16 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
+	ball=new paper
+
+	ballZone=new DustBin;
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 	Engine.run(engine);
   
@@ -30,8 +39,9 @@ function draw() {
   rectMode(CENTER);
   background(0);
   
-  drawSprites();
- 
+  ball.display(); 
+  ballZone.display(); 
+
 }
 
 
